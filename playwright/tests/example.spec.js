13c5da13954e1f7660fb2344ce0522d53fcc7fe8
Varun <varunrao2510@gmail.com>
2025-04-08 18:28:22 +0530

    new test case

---
+++ playwright/tests/example.spec.js
@@ -0,0 +1,28 @@
+// @ts-check
+
+import { test, expect } from '@playwright/test'
+
+test.describe('navigation', () => {
+    test.beforeEach(async ({ page }) => {
+        
+        await page.goto('https://playwright.dev/')
+    })
+
+
+test('has title', async ({ page }) => {
+    //await page.goto('https://playwright.dev/')
+
+    await expect(page).toHaveTitle(/Playwright/)
+})
+
+test('get started link', async ({ page }) => {
+    //await page.goto('https://playwright.dev/')
+
+    await page.getByRole('link', { name: 'Get started' }).click()
+    
+    // Expects page to have a heading with the name of Installation.
+
+    await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible()
+    
+})
+})
